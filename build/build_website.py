dbbc8bd0379c864553e877364f9f40799425e48e
Christos Athanasiadis <c.athanasiadis@uva.nl>
2022-02-24 11:11:55 +0100

    uvadl2c

--- build/build_website.py
+++ build/build_website.py
@@ -14,7 +14,7 @@ DEFAULT_DOCUMENT = "<a <!--$$LINK$$-->><i class='fa fa <!--$$ICON$$--> text-prim
 DEFAULT_RECORDING = "<a <!--$$LINK$$-->><i class='fa fa fa-video-camera text-primary'></i><span class='lecture-document'><!--$$NAME$$--></span></a></br>"
 DEFAULT_TA_NAME = "<a href='<!--$$LINK$$-->' target='_blank' style='color: white;'><!--$$NAME$$--></a>"
 DEFAULT_TA_PICTURE = "<a href='<!--$$LINK$$-->'><img class='img-circle' src='<!--$$IMAGE$$-->' hspace='5' width='120' alt='<!--$$NAME$$-->' title='<!--$$NAME$$-->'></a>"
-DEFAULT_PICTURE_FILENAME = "images/people/default-picture.png"
+DEFAULT_PICTURE_FILENAME = "images/people/default-picture.jpg"
 
 def _create_document_list(document_dict):
 	document_list = []
@@ -202,7 +202,7 @@ def build_TA_list(index_file,
 		
 
 	index_file = index_file.replace("<!--$$TA_PICTURES$$-->", "\n".join(TA_pic_list))
-	#pdb.set_trace()
+	pdb.set_trace()
 	return index_file
 
 
